bb42dde1643b0e421aaacae68c7f7386c9ac8f6f
Brian Walker <Asus@DESKTOP-JCRAKG5>
2019-10-06 15:12:30 -0300

    intermedio

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./sign-in.styles.scss";
+import SignInAndSignUpPage from "../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 class SigIn extends React.Component {
   constructor(props) {
@@ -17,7 +18,23 @@ class SigIn extends React.Component {
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password.</span>
+
+        <form>
+          <input name="email" type="email" value={this.state.email} required />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+          />
+          <label>Password</label>
+
+          <input type="submit" value="Submit Form"></input>
+        </form>
       </div>
     );
   }
 }
+
+export default SignIn;
