202680d655b33591d7255ecdd67b0ed89a8d787d
Brian German Walker <justbwalker@hotmail.com>
2019-10-09 20:40:25 -0300

    integrated firebase into application, created signin page with sigin in component and sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,10 +18,10 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select-account" });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => {
   console.log("algo");
-  return auth.signInWithPopup();
+  return auth.signInWithPopup(provider);
 };
 
 export default firebase;
