63b89718f4f3c3d1aeeabd9a4b56519f29c32a76
Brian German Walker <justbwalker@hotmail.com>
2019-10-13 23:10:39 -0300

    Capitulo 89

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import FormImput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+
+import "./sign-up.styles.scss";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  }
+}
